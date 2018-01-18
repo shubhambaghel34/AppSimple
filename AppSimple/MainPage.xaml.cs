5d61b6e74de0c9e847778b5c4b7d0db82135e25f
Enthusiastic_coder <21123907+shubhambaghel34@users.noreply.github.com>
2018-01-18 14:46:51 +0530

    Initial commit

---
+++ AppSimple/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppSimple
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MainPage= new NavigationPage(new Page1());
+        }
+    }
+}
---
+++ AppSimple/Page1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppSimple
+{
+    public class Page1 : ContentPage
+    {
+
+        public Page1()
+        {
+            var page1 = new ContentPage
+            {
+                Content = new TableView
+                {
+                    Intent = TableIntent.Form,
+                    Root = new TableRoot("Table Title") {
+                    new TableSection ("Section 1 Title") {
+                        new TextCell {
+                            Text = "TextCell Text",
+                            Detail = "TextCell Detail"
+                        },
+                        new EntryCell {
+                            Label = "EntryCell:",
+                            Placeholder = "default keyboard",
+                            Keyboard = Keyboard.Default
+                        }
+                    },
+                    new TableSection ("Section 2 Title") {
+                        new EntryCell {
+                            Label = "Another EntryCell:",
+                            Placeholder = "phone keyboard",
+                            Keyboard = Keyboard.Telephone
+                        },
+                        new SwitchCell {
+                            Text = "SwitchCell:"
+                        }
+                    }
+                }
+
+                }
+            };
+
+        }
+        
+    }
+}
